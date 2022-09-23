820a497026070f43d9c763c91409c1cfc18d80c5
GABRIELE GIOACCHINI <s23687@iisve.it>
2022-09-23 12:05:18 +0200

    Aggiungere i file di progetto.

---
+++ gestore ordini/ordine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestore_ordini
+{
+    class Ordine
+    {
+        public string idOrdine { get; set; }
+
+        public string codiceCliente { get; set; }
+
+        public string[] arrayArticoli { get; set; }
+
+        public Ordine(string codice, string cliente)
+        {
+            idOrdine = codice;
+            codiceCliente = cliente;
+            arrayArticoli = new string[1];
+        }
+
+        public bool insertArt(string codart) 
+        {
+            int i = arrayArticoli.Length;
+            arrayArticoli[i - 1] = codart;
+            arrayArticoli = new string[i + 1];
+            i++;  
+            return true;
+
+        
+        }
+
+    }
+}
